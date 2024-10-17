4c0dfe86d9fcb97595aafe1b38949778b7e2c51f
jeffj <jeffjeffisawesome@gmail.com>
2024-10-17 11:41:45 -0400

    "f"

--- messenger.py
+++ messenger.py
@@ -2,9 +2,10 @@ import os
 import pickle
 import string
 import cryptography
-from cryptography.hazmat.primitives.asymmetric import x25519
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives import hashes
+from cryptography import HKDF
 class MessengerServer:
     def __init__(self, server_signing_key, server_decryption_key):
         self.server_signing_key = server_signing_key
@@ -15,10 +16,14 @@ class MessengerServer:
         return
 
     '''
-    Hash and Sign the Client's public key using the server_signing_key using ECDSA, and return it, along with the name in front.
+    cert: name, public key
+
+    Hash and Sign the Client's public key using the server_signing_key using ECDSA, and return it.
+
+    Returns: Signature of public Key
     '''
     def signCert(self, cert):
-        return cert[0], self.server_signing_key.sign(cert[1], ec.ECDSA(hashes.SHA256))
+        return self.server_signing_key.sign(cert[1], ec.ECDSA(hashes.SHA256))
 
 class MessengerClient:
 
@@ -31,33 +36,43 @@ class MessengerClient:
 
     '''
     Generates a Private Key and Public Key using x25519, saves both, and sends over the name of the messenger, along with the public key.
+    Returns: Name, Public Key
     '''
     def generateCertificate(self):
-        self.private_key = x25519.X25519PrivateKey.generate()
+        self.private_key = X25519PrivateKey.generate()
         self.public_key = self.private_key.public_key()
         return self.name, self.public_key
 
     '''
-    certificate = name, signed public key of certificate owner.
-    Uses "Verify" to check to see if the certificate was signed by the server. If there are no exceptions, add the name to self.conns, and add the public key to certs.
+    certificate: name, public key
+    signature: result of signCert(certificate)
+
+    Uses "Verify" to check to see if the certificate was signed by the server.
+    
+    Returns: Nothing
     '''
     def receiveCertificate(self, certificate, signature):
         try:
             self.server_signing_pk.verify(signature, certificate[1], ec.ECDSA(hashes.SHA256()))
         except:
             print("CANNOT VERIFY SIGNATURE")
-        self.conns.add(certificate[0])
-        self.certs.add(certificate[1])
+        self.certs[certificate[0]] = certificate[1]#adds to dictionary the name to the public key. We can update the key later.
+        #The root_key is the shared secret
+        self.root_key = self.private_key.exchange(certificate[1])
+        
+    '''
+    rk: root key
+    dh_out: previous dh_out from the previous operation of kdf using DH_output as "in" and chain key as "key"
+    '''
+    def KDF_RK(self, rk, dh_out):
+        return HKDF(algorithm=hashes.SHA256(), length=64, salt=rk,info=b'Derivation of Root Key').derive(dh_out)
+
 
     def sendMessage(self, name, message):
         raise Exception("not implemented!")
         return
 
-    '''
-    name = self.name
-    header = sender's current ratchet public key
-    ciphertext: ciphertext of the sender's message
-    '''
+
     def receiveMessage(self, name, header, ciphertext):
         raise Exception("not implemented!")
         return
