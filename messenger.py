d392ab8ec53f8ec9b1bc521df22eedc88830c08a
jeffj <jeffjeffisawesome@gmail.com>
2024-10-17 21:43:37 -0400

    Added initializers for sendMessage and receiveMessage: have not completed the functions

--- messenger.py
+++ messenger.py
@@ -56,24 +56,56 @@ class MessengerClient:
             self.server_signing_pk.verify(signature, certificate[1], ec.ECDSA(hashes.SHA256()))
         except:
             print("CANNOT VERIFY SIGNATURE")
-        self.certs[certificate[0]] = certificate[1]#adds to dictionary the name to the public key. We can update the key later.
-        #The root_key is the shared secret
-        self.root_key = self.private_key.exchange(certificate[1])
+        self.certs[certificate[0]] = certificate[1]#adds to dictionary the name, pointing to it's corresponding public key
+        #we can generate the shared key when we need it later.
         
     '''
     rk: root key
     dh_out: previous dh_out from the previous operation of kdf using DH_output as "in" and chain key as "key"
+    Output: 64 bytes, which will be split into 32, 32, one used for the new rk, and for the chain key
     '''
-    def KDF_RK(self, rk, dh_out):
-        return HKDF(algorithm=hashes.SHA256(), length=64, salt=rk,info=b'Derivation of Root Key').derive(dh_out)
+    def KDF_RK(rk, dh_out):
+        return HKDF(algorithm=hashes.SHA256(), length=64, salt=rk,info=b'KDF_RK_HE').derive(dh_out)
 
+    '''
+    name: receiver's name (who we want to send to)
+    message: message
 
+    returns: header, ciphertext
+    '''
     def sendMessage(self, name, message):
+        #Initialize, if we haven't sent a message yet
+        #Section 3.3 of Signal Doc
+        if self.conns[name] == None:
+            #Name -> Another dictionary containing values {DHs, DHr, RK, CKs, CKr, Ns, Nr, PN, MKSKIPPED}
+            #To access/edit values, self.conns[name]['DHs'], for example
+            DHs = X25519PrivateKey.generate()
+            DHr = self.certs[name] #Receiver's public key
+            shared_key = self.private_key.exchange(DHr)
+            RK, CKs = self.KDF_RK(shared_key, DHs.exchange(DHr))
+            self.conns[name] = {'DHs': DHs, 'DHr': DHr, 'RK': RK, 'CKs': CKs, 'CKr': None, 'Ns': 0, 'Nr': 0, 'PN': 0, 'MKSKIPPED': {}}
+        
+        #TODO
         raise Exception("not implemented!")
         return
 
-
+    '''
+    name: name of the sender
+    header (Section 2.6): contains the message's number in the sending chain (N = 0, 1, 2, ...) and length in the previous sending chain
+    ciphertext: ciphertext
+    
+    returns: the original message
+    '''
     def receiveMessage(self, name, header, ciphertext):
+        #Initialized according to section 3.3 of Signal Spec
+        if self.conns[name] == None:
+            #To access/edit values, do self.conns[name]['DHs'], for example
+            DHs = self.private_key
+            RK = self.private_key.exchange(self.certs[name]) #shared_key
+            self.conns[name] = {'DHs': DHs, 'DHr': None, 'RK': RK, 'CKs': None, 'CKr': None, 'Ns': 0, 'Nr': 0, 'PN': 0, 'MKSKIPPED': {}}
+        
+
+        #TODO
         raise Exception("not implemented!")
         return
 
